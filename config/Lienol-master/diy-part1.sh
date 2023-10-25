3906850415f3bc733545a4d9b04b7a36ff652eb2
wylcloud <82373611+wylcloud@users.noreply.github.com>
2023-10-25 17:46:22 +0800

    Update diy-part1.sh

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -8,7 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
